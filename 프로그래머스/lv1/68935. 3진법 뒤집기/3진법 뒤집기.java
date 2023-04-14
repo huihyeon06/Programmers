2e6713718d13ca4753189caa81f4b1faf4f6dc52
huihyeon06 <113982023+huihyeon06@users.noreply.github.com>
2023-04-15 01:28:48 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.10 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str=Integer.toBinaryString(n);
+        str=Integer.toString(n, 3);
+        StringBuffer s = new StringBuffer(str);
+        str=s.reverse().toString();
+        answer=Integer.parseInt(str, 3);
+        
+        return answer;
+    }
+}
