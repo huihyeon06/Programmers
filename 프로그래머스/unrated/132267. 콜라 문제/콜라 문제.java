c423691244f4866002962e2611739986f3343c91
huihyeon06 <113982023+huihyeon06@users.noreply.github.com>
2023-06-09 00:46:47 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(n/a>0){
+            answer+=(n/a)*b;
+            n=(n/a)*b+n%a;
+            if(n==1){
+                break;
+            }
+        }
+        return answer;
+    }
+}
