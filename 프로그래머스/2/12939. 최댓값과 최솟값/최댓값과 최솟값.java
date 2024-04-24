3df75162049305cc5514a539888c265b280500bd
huihyeon06 <113982023+huihyeon06@users.noreply.github.com>
2024-04-24 22:53:40 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.19 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s) {
+        String[] ss = s.split(" ");
+        int[] si =  new int[ss.length];
+        for(int i=0; i<si.length; i++){
+            si[i]=Integer.parseInt(ss[i]);
+        }
+        int max=si[0], min = si[0];
+        for(int i=0; i<si.length; i++){
+            if(si[i]>max){
+                max=si[i];
+            }else if(si[i]<min){
+                min=si[i];
+            }
+        }
+        String answer = String.valueOf(min+" "+max);
+        
+        return answer;
+    }
+}
